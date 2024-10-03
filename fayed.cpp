55cb22bfa919ddf83c5fa1b77586f7c56389df0b
fayed <fayedhasan1000@gmail.com>
2024-10-04 00:11:36 +0600

    first time commit but not the last insallah

---
+++ fayed.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ln endl
+#define pb push_back
+#define po pop_back
+#define lop(i, a, b) for (ll i = a; i <= b; i++)
+#define lup(i, a, b) for (ll i = a; i >= b; i--)
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+void bz()
+{
+    cout << 13233;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+        bz();
+    return 0;
+}
